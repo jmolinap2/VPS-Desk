b9d18a55ca1ede431fdcee6202a3aba2a2a29c7c
Jesús Alejandro Molina Peñaherrera <68759201+jmolinap2@users.noreply.github.com>
2026-09-12 13:59:34 -0500

    refactor(ui): add containers view code-behind

---
+++ src/VpsDesk.Desktop/Views/ContainersView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace VpsDesk.Desktop.Views;
+
+public partial class ContainersView : UserControl
+{
+    public ContainersView()
+    {
+        InitializeComponent();
+    }
+}

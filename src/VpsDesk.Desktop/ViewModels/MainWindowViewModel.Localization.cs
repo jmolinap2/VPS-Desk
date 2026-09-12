bbda8e138ca785e9771899185d9ee5b16830633a
Jesús Alejandro Molina Peñaherrera <68759201+jmolinap2@users.noreply.github.com>
2026-09-12 15:50:48 -0500

    feat(localization): refresh shell bindings after language change

--- src/VpsDesk.Desktop/ViewModels/MainWindowViewModel.Localization.cs
+++ src/VpsDesk.Desktop/ViewModels/MainWindowViewModel.Localization.cs
@@ -7,6 +7,12 @@ public partial class MainWindowViewModel
     public string LocalizedHeaderTitle => LocalizationService.T($"Nav_{SelectedPage}");
     public string LocalizedHeaderSubtitle => LocalizationService.T($"Header_{SelectedPage}");
 
+    public void RefreshLocalization()
+    {
+        OnPropertyChanged(nameof(LocalizedHeaderTitle));
+        OnPropertyChanged(nameof(LocalizedHeaderSubtitle));
+    }
+
     partial void OnHeaderTitleChanged(string value)
         => OnPropertyChanged(nameof(LocalizedHeaderTitle));
 

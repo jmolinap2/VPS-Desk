0b49907ddb95e284057bbc96a4f9dda14577c1a8
Jesús Alejandro Molina Peñaherrera <68759201+jmolinap2@users.noreply.github.com>
2026-09-13 10:06:34 -0500

    feat(projects): add projects view code-behind

---
+++ src/VpsDesk.Desktop/Views/ProjectsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace VpsDesk.Desktop.Views;
+
+public partial class ProjectsView : UserControl
+{
+    public ProjectsView()
+    {
+        InitializeComponent();
+    }
+}
